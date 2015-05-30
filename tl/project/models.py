d643f4d0fbbe6026a1927a661606d688c02425f9
Tom Leo <tom@tomleo.com>
2015-05-29 20:30:08 -0400

    Added projects app

---
+++ tl/project/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Technology(models.Model):
+    """
+    name
+    description
+
+    Reverse relations can be used to compute experience of the lanuage
+
+    This class should be abstract
+    """
+    pass
+
+class Category(Technology):
+    pass
+
+class Framework(Technology):
+    pass
+
+class Lanuages(Technology):
+    pass
+
+class Project(models.Model):
+    """
+    name
+    languages -> one-to-many Lanuage
+    categories -> one-to-many Category
+    frameworks -> one-to-many Framework
+    """
+    pass
+
