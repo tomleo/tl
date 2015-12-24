5f59348428635bb41879ac1996d5b5653bfed4c1
Tom <tom@tomleo.com>
2015-12-24 11:56:24 -0500

    Remove bower requirement
    - Commit bower_components to repo
    - Add styles to anchor tags
    - Add link icon to external links

--- frontend/dist/app.js
+++ frontend/dist/app.js
@@ -1,7 +1,24 @@
-"use strict";
+'use strict';
 
-var arr = [1, 2, 3];
-console.log(arr.map(function (x) {
-  return x * x;
-}));
-console.log("Greetings from es6");
+var site_url = window.location.hostname;
+
+var getLocation = function getLocation(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
+
+function add_external_link(jq_a) {
+    var _l = getLocation(jq_a.attr('href'));
+    if (_l.hostname !== site_url) {
+        jq_a.append(' <i class="fa fa-external-link"></i>');
+    }
+}
+
+$('#main a').each(function () {
+    add_external_link($(this));
+});
+
+$('footer a').each(function () {
+    add_external_link($(this));
+});
